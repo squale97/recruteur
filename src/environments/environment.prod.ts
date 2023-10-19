b4c32feb915941571c0f45e946b508cb0484957e
Aristideoued <ouedraogoaris@gmail.com>
2023-10-19 16:13:43 +0000

    evolution pour deploiement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,10 @@
+
 export const environment = {
   production: true,
-  baseUrl: 'http://172.21.12.31:5200/',
+  baseUrl: "https://ecvback.codingagain.com/",
+  passAuth: "R#9sFvP2T@LmG$7zE1YqXcA6wDnB5hZtO0jKlU3eIgH8fN4pM",
+
+  userAuth:"management.admin@codingagain.com"
+ 
+  
 };
